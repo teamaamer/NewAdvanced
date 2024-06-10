ce0af25228099fa9290b8559bb7e7b8855ded3dd
teamaamer <145798330+teamaamer@users.noreply.github.com>
2024-06-11 00:14:09 +0300

    Update index.js
    Updated

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const app = express();
 
 app.use(express.json());
 
-
 import userRoutes from './routes/userRoutes.js';
 import exchange from './routes/exchangeRoutes.js';
 import gardenRouter from './routes/gardenRoutes.js';
-import guideRouter from './routes/guideRouter.js'; // Corrected import statement
-import plotRoutes from './routes/plotRoutes.js'; // Import plot routes
+import guideRouter from './routes/guideRouter.js';
 
 app.use('/users', userRoutes);
 app.use('/exchange', exchange);
 app.use('/gardens', gardenRouter);
-app.use('/guides', guideRouter); // Use guideRouter instead of router
-app.use('/plots', plotRouter); // Add plot routes
+app.use('/guides', guideRouter);
+
+// Mount the plot routes under the gardens routes
+app.use('/gardens/:gardenId/plots', require('./routes/plotRoutes'));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
