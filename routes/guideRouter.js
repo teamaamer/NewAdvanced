a825b14677762d8475ac2a3299ba3ddd2270884c
teamaamer <145798330+teamaamer@users.noreply.github.com>
2024-06-14 20:34:13 +0300

    Update guideRouter.js

--- routes/guideRouter.js
+++ routes/guideRouter.js
@@ -5,12 +5,12 @@ import authMiddleware from '../middleware/auth.js';
 
 const guideRouter = express.Router();
 
-guideRouter.post('/', authMiddleware, addGuide);
-guideRouter.get('/', authMiddleware, getAllGuides);
-guideRouter.get('/:id', authMiddleware, getGuideById);
-guideRouter.get('/title/:title', authMiddleware, getGuideByTitle);
-guideRouter.put('/:id', authMiddleware, updateGuide);
-guideRouter.delete('/:id', authMiddleware, deleteGuide);
+guideRouter.post('/addguide', authMiddleware, addGuide);
+guideRouter.get('/getguides', authMiddleware, getAllGuides);
+guideRouter.get('/getguidesid/:id', authMiddleware, getGuideById);
+guideRouter.get('/getguidestt', authMiddleware, getGuideByTitle);
+guideRouter.put('/getguidesupda/:id', authMiddleware, updateGuide);
+guideRouter.delete('/deleteguide/:id', authMiddleware, deleteGuide);
 
 export default guideRouter;
 
